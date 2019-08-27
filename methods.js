64ccf1dbbeaaae56dc2bb14b339295e7984cd6a8
Aybson Mercado <aybsonmg@gmail.com>
2019-08-26 21:01:27 -0600

    Se agrega funcion mostrarTodos

--- methods.js
+++ methods.js
@@ -1,3 +1,19 @@
 import { users } from './users.js';
 
-console.log(users);
+function mostrarTodos(){
+    
+    const table = document.getElementById("table-body");
+    console.log(table);
+
+    users.forEach(function(user){
+        console.log(user.name);
+        var string = "<tr>";
+        string += "<td>"+ user.name + " " + user.lastname +"</td>";
+        string += "<td>"+ user.age +"</td>";
+        string += "<td>"+ user.name +"</td>";
+        table.innerHTML += string;
+    });
+
+}
+
+mostrarTodos();
